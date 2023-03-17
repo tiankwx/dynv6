8f2dcb3a30c6b734dac5e929e7070fa598092972
tiankwx <tiankwx@outlook.com>
2023-03-17 14:58:09 +0800

    待测试；

--- dynv6.sh
+++ dynv6.sh
@@ -21,39 +21,14 @@ if [ -z "$types" ]; then
     types=ip
 fi
 
-logfile="/var/log/dynv6"
-# -d 参数判断 $logfile 是否存在
-[ -d $logfile ] || mkdir -p $logfile
-
-# 原IP文件
-file4=$logfile/$1.ipv4.data
-[ -e $file4 ] && old4=$(cat $file4)
-if [[ -n $old4]]; then
-    echo -e "本地ipv4缓存地址"
-    echo -e $old4
-fi
-
-file6=$logfile/$1.IPv6.data
-[ -e $file6 ] && old=$(cat $file6)
-if [[ -n $old]]; then
-    echo -e "本地ipv6缓存地址"
-    echo -e $old
-fi
-
 # -n 字符串	字符串的长度不为零则为真
 if [ -n "$device" ]; then
     device="dev $device"
 fi
 
-address6=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
-# address6=$(curl -s ipv6.miuku.net)
-echo -e "本地网卡地址"
-echo -e $address6
-
-address4=$(curl -m 15 -s http://ipv4.miuku.net/)
-echo -e "本地ipv4地址"
-echo -e $address4
-echo $address4 >$file4
+logfile="/var/log/dynv6"
+# -d 参数判断 $logfile 是否存在
+[ -d $logfile ] || mkdir -p $logfile
 
 if [ -e /usr/bin/curl ]; then
     bin="curl -fsS"
@@ -65,38 +40,93 @@ else
     exit 1
 fi
 
-if [ -z "$address6" ]; then
-    echo "no IPv6 address found"
-    echo "没有找到IPv6地址，重启测试"
-    # /usr/sbin/reboot
-    exit 1
+file4=$logfile/$1.ipv4.data
+file6=$logfile/$1.IPv6.data
+
+get_ipv4() {
+    address4=$(curl -m 15 -s http://ipv4.miuku.net/)
+    if [ -z "$address4" ]; then
+        echo "no IPv4 address found"
+        echo "没有找到IPv4地址"
+        return 1
+    fi
+    echo $address4 >$file4
+}
+
+get_ipv6() {
+    address6=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
+    if [ -z "$address6" ]; then
+        echo "no IPv6 address found"
+        echo "没有找到IPv6地址"
+        return 1
+    fi
+    echo $address6 >$file6
+}
+
+update_ipv6_address() {
+    get_ipv6
+
+    [ -e $file6 ] && old=$(cat $file6)
+    if [[ -n $old]]; then
+        echo -e "本地ipv6缓存地址"
+        echo -e $old
+    fi
+
+    # 取解析地址
+    remote=$(/usr/bin/nslookup $hostname 119.29.29.29 | grep Address: | tail -n1 | cut -d ' ' -f2)
+    echo -e "nslookup解析地址"
+    echo -e $remote
+
+    if [ "$old" = "$address6" ] && [ "$remote" = "$address6" ]; then
+        echo -e $(date +"%Y-%m-%d %H:%M:%S") >>$logfile/$1.IPv6.log
+        echo -e "IPv6 address unchanged" | tee -a $logfile/$1.IPv6.log
+        echo -e "IPv6 没有改变\n" | tee -a $logfile/$1.IPv6.log
+        return 1
+    fi
+
+    # 域名 类型 地址 token 日志路径 保存路径
+    update $hostname ipv6 $address6 $token $logfile $file6
+}
+
+update_ipv4_address() {
+    get_ipv4
+
+    [ -e $file4 ] && old4=$(cat $file4)
+    if [[ -n $old4]]; then
+        echo -e "本地ipv4缓存地址"
+        echo -e $old4
+    fi
+
+    if [ "$old4" = "$address4" ]; then
+        echo -e $(date +"%Y-%m-%d %H:%M:%S") >>$logfile/$1.IPv4.log
+        echo -e "IPv4 address unchanged" | tee -a $logfile/$1.IPv4.log
+        echo -e "IPv4 没有改变\n" | tee -a $logfile/$1.IPv4.log
+        return 1
+    fi
+    # 域名 类型 地址 token 日志路径 保存路径
+    update $hostname ipv4 $address4 $token $logfile $file4
+}
+
+update() {
+    url="https://dynv6.com/api/update?zone=$1&$2=$3&token=$4"
+    echo $url
+    # send addresses to dynv6
+    logpath=$5/$1.$2.log
+    echo -e $(date +"%Y-%m-%d %H:%M:%S") >>$logpath
+    echo -e "新IP:$3" >>$logpath
+    echo -e $url >>$logpath
+    echo -e "更新状态：" >>$logpath
+    $bin $url | tee -a $logpath
+    echo -e "\n" | tee -a $logpath
+    # save address
+    echo $3 >$6
+}
+
+if [ $types == "ipv4" ]; then
+    update_ipv4_address
+elif [ $types == "ipv6" ]; then
+    update_ipv6_address
+else
+    update_ipv4_address
+    update_ipv6_address
 fi
-
-$bin "https://dynv6.com/api/update?zone=$hostname&ipv4=$address4&token=$token"
-echo ""
-
-# 取解析地址
-remote=$(/usr/bin/nslookup $hostname 119.29.29.29 | grep Address: | tail -n1 | cut -d ' ' -f2)
-echo -e "nslookup解析地址"
-echo -e $remote
-
-if [ "$old" = "$address6" ] && [ "$remote" = "$address6" ]; then
-    echo -e $(date +"%Y-%m-%d %H:%M:%S") >>$logfile/$1.IPv6.log
-    echo -e "IPv6 address unchanged" | tee -a $logfile/$1.IPv6.log
-    echo -e "IPv6 没有改变\n" | tee -a $logfile/$1.IPv6.log
-    exit
-fi
-
-url="https://dynv6.com/api/update?zone=$hostname&ipv6=$address6&token=$token"
-echo $url
-# send addresses to dynv6
-echo -e $(date +"%Y-%m-%d %H:%M:%S") >>$logfile/$1.IPv6.log
-echo -e "新IP:$address6" >>$logfile/$1.IPv6.log
-echo -e $url >>$logfile/$1.IPv6.log
-echo -e "更新状态：" >>$logfile/$1.IPv6.log
-$bin $url | tee -a $logfile/$1.IPv6.log
-echo -e "\n" | tee -a $logfile/$1.IPv6.log
-# $bin "http://ipv4.dynv6.com/api/update?hostname=$hostname&ipv4=auto&token=$token"
-
-# save address
-echo $address6 >$file6
